4460a1bd207a2b72cc96e65fc937defe713ce529
Anwesh43 <anweshthecool0@gmail.com>
2020-01-21 11:35:48 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbarstepsideview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbarstepsideview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedbarstepsideview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.barstepsideview.BarStepSideView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BarStepSideView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
