64fd79340942931050cacee3414a741cf8c7d0f6
Anwesh43 <anweshthecool0@gmail.com>
2020-01-21 11:16:16 +0530

    adding Canvas extension methoid

--- barstepsideview/src/main/java/com/anwesh/uiprojects/barstepsideview/BarStepSideView.kt
+++ barstepsideview/src/main/java/com/anwesh/uiprojects/barstepsideview/BarStepSideView.kt
@@ -24,3 +24,29 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBarStep(j : Int, scale : Float, size : Float, sj : Float, paint : Paint) {
+    val hGap : Float = 2 * size / bars
+    save()
+    translate(0f, -size + hGap * j + hGap / 2)
+    drawRect(RectF(-size, -hGap / 2, size, hGap / 2), paint)
+    restore()
+}
+
+fun Canvas.drawBarSteps(scale : Float, size : Float, sj : Float, paint : Paint) {
+    for (j in 0..(bars - 1)) {
+        drawBarStep(j, scale, size, sj, paint)
+    }
+}
+
+fun Canvas.drawBSSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawBarSteps(scale, size, 1f - 2 * i, paint)
+    restore()
+}
