4a8f0cb32afbf30f3c0feb0370c928bd5cf57fce
Anwesh43 <anweshthecool0@gmail.com>
2020-01-21 11:07:34 +0530

    adding constants andd libraries

--- barstepsideview/src/main/java/com/anwesh/uiprojects/barstepsideview/BarStepSideView.kt
+++ barstepsideview/src/main/java/com/anwesh/uiprojects/barstepsideview/BarStepSideView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.barstepsideview
 /**
  * Created by anweshmishra on 21/01/20.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val bars : Int = 3
+val scGap : Float = 0.02f / bars
+val delay : Long = 20L / bars
+val sizeFactor : Float = 2.2f
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
