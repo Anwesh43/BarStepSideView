5490366d74951ba289c2038517eecac5eceb7026
Anwesh43 <anweshthecool0@gmail.com>
2020-01-21 11:18:37 +0530

    adding BarStepSideView class

--- barstepsideview/src/main/java/com/anwesh/uiprojects/barstepsideview/BarStepSideView.kt
+++ barstepsideview/src/main/java/com/anwesh/uiprojects/barstepsideview/BarStepSideView.kt
@@ -50,3 +50,21 @@ fun Canvas.drawBSSNode(i : Int, scale : Float, paint : Paint) {
     drawBarSteps(scale, size, 1f - 2 * i, paint)
     restore()
 }
+
+class BarStepSideView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
